2497654b021a8a53744cc7ba8c1eae9c420d410c
Masa Morishita <masam@masamorishita.com>
2019-05-25 22:16:54 +0900

    Modify requestMessageOwnershipVerification function

--- src/blockchain.js
+++ src/blockchain.js
@@ -96,7 +96,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            let message = '${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry';
+            let message = `${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`;
             resolve(message);
         });
     }
